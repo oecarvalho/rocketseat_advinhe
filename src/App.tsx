dabd6f97ed4b67db389afcb59685146f56b0dd4f
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-04-27 17:57:23 -0300

    finalizando header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import styles from './app.module.css';
+import { Header } from "./components/Header";
 
 
 function App() {
 
+  function handlerRestartGame(){
+    alert("reiniciar o jogo")
+  }
 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.container}>
+      <main>
+        <Header current = {5} max = {10} onRestart={handlerRestartGame} />
+      </main>
     </div>
   )
 }
