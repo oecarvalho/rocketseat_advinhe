ffc0f4b32caac4f67b5b3dadc77ce14b85c95c48
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-04-27 21:14:26 -0300

    adicionando campo de dicas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './app.module.css';
 import { Header } from "./components/Header";
+import { Tip } from './components/Tip';
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
     <div className={styles.container}>
       <main>
         <Header current = {5} max = {10} onRestart={handlerRestartGame} />
+        
+        <Tip tip='Uma das linguagens de programação mais utilizadas'/>
       </main>
     </div>
   )
