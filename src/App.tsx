d5dcac72072b17ac8b6b424f49de4035f7bbd40a
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-04-28 19:55:15 -0300

    criando componentes de letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './app.module.css';
 import { Header } from "./components/Header";
 import { Tip } from './components/Tip';
-
+import { Letter } from './components/Letter';
 
 function App() {
 
@@ -13,8 +13,15 @@ function App() {
     <div className={styles.container}>
       <main>
         <Header current = {5} max = {10} onRestart={handlerRestartGame} />
-        
         <Tip tip='Uma das linguagens de programação mais utilizadas'/>
+
+        <div className={styles.word}>
+          <Letter value='r'/>
+          <Letter value='e'/>
+          <Letter value='a'/>
+          <Letter value='c'/>
+          <Letter value='t'/>
+        </div>
       </main>
     </div>
   )
---
+++ src/components/Letter/index.tsx
@@ -0,0 +1,14 @@
+
+import style from './style.module.css'
+
+type Props ={
+    value?: string
+}
+
+export function Letter({value = ''}: Props){
+    return(
+        <div className={style.letter}>
+            <span>{value}</span>
+        </div>
+    )
+}
